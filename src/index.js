0f226a50fc067d01150777f1c3535520c4a86b88
datboiluiskrrt <datboiluiskrrt@gmail.com>
2021-11-30 13:48:28 -0600

    Adding the second event handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener(
+  "mouseover",
+  (evt) => {
+    evt.target.style.color = "red";
+
+    setTimeout(() => {
+      evt.target.style.color = "";
+    }, 500);
+  },
+  false
+);
+
+window.addEventListener("keydown", checkKeyPress, false);
+function checkKeyPress(key) {
+  if (key.keyCode == "65") {
+    alert(`The 'a' letter key has been pressed!`);
+  }
+}
